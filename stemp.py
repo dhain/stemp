436570e523a3fc89892d845d5f2c9314f7ec9068
David Hain <dhain@webskewer.org>
2008-07-14 23:31:14 -0700

    use more iteration

--- stemp.py
+++ stemp.py
@@ -1,54 +1,69 @@
 from itertools import imap
 
+
+def _collapse(it):
+    if isinstance(it, basestring):
+        yield it
+    else:
+        try:
+            for it2 in imap(_collapse, it):
+                for el in it2:
+                    yield el
+        except TypeError:
+            yield it
+
+
 __all__ = ['htmldoc']
 
+
 def htmldoc(doctype, html, encoding='utf-8'):
-    return ''.join(('<!DOCTYPE ',' '.join(imap(str, doctype)),'>',
-        unicode(html).encode(encoding)))
+    yield '<!DOCTYPE ' + ' '.join(imap(str, doctype)) + '>'
+    if encoding is None:
+        for part in html:
+            yield part
+    else:
+        for part in html:
+            yield part.encode(encoding)
+
 
 class htmltag(object):
     name = u''
-    attrs = {}
     
-    def _parts(self, args):
+    def __init__(self, attrs=None):
+        self.attrs = {} if attrs is None else attrs
+    
+    def _open(self, empty=False):
         yield u'<'
         yield self.name
         for x in self.attrs.iteritems():
             yield u' %s="%s"' % x
-        if args:
-            yield u'>'
-            for a in args:
-                yield a
-            yield u'</'
-            yield self.name
-            yield u'>'
-        else:
+        if empty:
             yield u' />'
+        else:
+            yield u'>'
     
-    def _render(self, args=()):
-        return u''.join(self._parts(args))
+    def render(self, children=()):
+        yield u''.join(self._open(not children))
+        if children:
+            for part in _collapse(children):
+                yield unicode(part)
+            yield u'</' + self.name + '>'
     
     @classmethod
     def __call__(cls, **kw):
-        i = cls()
-        i.attrs = dict((k.rstrip('_'), v) for k, v in kw.iteritems())
-        return i
+        return cls(dict((k.rstrip('_'), v) for k, v in kw.iteritems()))
     
     def __unicode__(self):
-        return self._render()
+        return u''.join(self)
     
-    def __getitem__(self, args):
-        if isinstance(args, basestring):
-            a = (unicode(args),)
-        else:
-            try:
-                a = imap(unicode, args)
-            except TypeError:
-                a = (unicode(args),)
-        return self._render(a)
+    def __getitem__(self, children):
+        if isinstance(children, basestring):
+            children = ((unicode(children),),)
+        return self.render(children)
     
     def __iter__(self):
-        raise TypeError('%r object is not iterable' % (type(self)))
+        return self.render()
+
 
 def _mktag(name_):
     class a(htmltag):
@@ -56,15 +71,21 @@ def _mktag(name_):
     a.__name__ = name_
     return name_, a()
 
+
 tags = ('html','head','title','body','link','script','h1','p')
 locals().update(dict(imap(_mktag, tags)))
 __all__.extend(tags)
 
+
 if __name__ == '__main__':
-    print html[
-        head[
-            title['the title!'],
-            link(href='/styles.css', rel='stylesheet')
-        ],
-        body(class_='the_class')[1]
-    ]
+    print ''.join(htmldoc(('html', 'PUBLIC', '"-//W3C//DTD HTML 4.01//EN"',
+                           '"http://www.w3.org/TR/html4/strict.dtd"'),
+        html[
+            head[
+                title['the title!'],
+                link(href='/styles.css', rel='stylesheet'),
+                link(href='/main.css', rel='stylesheet')
+            ],
+            body(class_='the_class')[1]
+        ]
+    ))
